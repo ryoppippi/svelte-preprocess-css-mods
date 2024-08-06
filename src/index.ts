ab8e6f0987dc7e8bc10d1d98a2dac45a439235de
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-06 20:47:26 +0100

    feat: add css module support and refactor utils
    This commit introduces support for CSS modules in the Svelte
    preprocessor. It also includes a significant refactor of the utils
    module. The alias loading function has been moved to its own file
    (alias.ts) and the CSS module related functions have been moved to
    css-module.ts. The old utils.ts file has been removed. The package.json
    and pnpm-lock.yaml files have been updated to include new dependencies
    required for the CSS module support.

---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import MagicString from 'magic-string';
+import type { PreprocessorGroup } from 'svelte/compiler';
+import { findStaticImports } from 'mlly';
+import { genObjectFromRaw } from 'knitwork';
+import { loadAliases } from './utils/alias';
+import type { CssModule } from './utils/css-module';
+import { getCssModule, getCssModuleImports } from './utils/css-module';
+
+// TODO: improve tree-shaking for production build
+
+export function cssModules(): PreprocessorGroup {
+	const cssModuleCache = new Map<string, CssModule[]>();
+	return {
+		async script({ content, filename }) {
+			const aliases = await loadAliases();
+			const s = new MagicString(content);
+
+			/* get css/scss module imports */
+			const imports = findStaticImports(content).filter(
+				i => ['module.css', 'module.scss'].some(ext => i.specifier.endsWith(ext)),
+			);
+
+			/* find css/scss module imports */
+			const cssModuleImpots
+				= await getCssModuleImports({
+					imports,
+					aliases,
+					filename,
+				});
+
+			/* transform css/scss modules */
+			const cssModules = [];
+			for (const cmi of cssModuleImpots) {
+				const cssModule = await getCssModule(cmi);
+				cssModules.push(cssModule);
+
+				/* generate css module exports */
+				const obj = genObjectFromRaw(cssModule.exports);
+				const gen = `const ${cmi.defaultImport} = ${obj};`;
+				s.overwrite(cmi.imp.start, cmi.imp.end, gen);
+			}
+			if (filename != null) {
+				cssModuleCache.set(filename, cssModules);
+			}
+
+			return {
+				code: s.toString(),
+				map: s.generateMap({
+					source: filename,
+					includeContent: true,
+				}),
+			};
+		},
+		style({ content, filename }) {
+			const s = new MagicString(content);
+
+			/* if filename is null, it's a svelte component */
+			if (filename == null) {
+				return { code: content };
+			}
+
+			const cssModules = cssModuleCache.get(filename) ?? [];
+
+			/* append css module styles */
+			for (const cssModule of cssModules) {
+				const code = `
+/* ${cssModule.path} */
+${cssModule.css}
+`;
+				s.append(code);
+			}
+
+			return {
+				code: s.toString(),
+				map: s.generateMap({
+					source: filename,
+					includeContent: true,
+				}),
+			};
+		},
+	};
+}
---
+++ src/utils/alias.ts
@@ -0,0 +1,40 @@
+import type { Config as SvelteKitConfig } from '@sveltejs/kit';
+import type { UserConfig as ViteConfig } from 'vite';
+import { loadConfig } from 'unconfig';
+
+export function addSlash(str: string): string {
+	return (str.endsWith('/') ? str : `${str}/`);
+}
+
+export async function loadAliases(): Promise<Record<string, string>> {
+	const { config } = await loadConfig({
+		merge: true,
+		sources: [
+			{
+				files: 'svelte.config',
+				rewrite: (_config) => {
+					const config = _config as SvelteKitConfig;
+					return { alias: config?.kit?.alias };
+				},
+			},
+			{
+				files: 'vite.config',
+				rewrite: (_config) => {
+					const config = _config as ViteConfig;
+					return { alias: config?.resolve?.alias };
+				},
+			},
+		],
+	});
+	const _alias = config?.alias ?? {};
+
+	const alias: Record<string, string> = {};
+
+	for (const [key, value] of Object.entries(_alias)) {
+		if (typeof value === 'string' && typeof key === 'string') {
+			alias[key] = addSlash(value);
+		}
+	}
+
+	return alias;
+}
