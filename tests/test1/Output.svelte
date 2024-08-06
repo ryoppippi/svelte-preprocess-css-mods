3992fcb4c31c1a2d8d03875e4d14873a571f7566
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-06 22:35:16 +0100

    feat(test): add Output.svelte

---
+++ tests/test1/Output.svelte
@@ -0,0 +1,27 @@
+<script>
+	const s = {
+		error: 's-Lwza_error',
+		success: 's-Lwza_success',
+	};
+</script>
+
+<div class={s.error}>
+	hello
+</div>
+
+<div class={s.success}>
+	world
+</div>
+
+<style>
+
+/* /Users/ryoppippi/ghq/github.com/ryoppippi/svelte-preprocess-css-modules/tests/assets/class.module.css */
+.s-Lwza_error {
+  color: red;
+}
+
+.s-Lwza_success {
+  color: green;
+}
+
+</style>
