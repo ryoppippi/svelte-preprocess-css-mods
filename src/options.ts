09f3ee44c694c5c8522af506768217539098dc7b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-07 13:40:09 +0100

    feat(options): add mode option and make all options required
    This commit introduces a new 'mode' option to the Options type, which
    allows users to choose between 'object' and 'underscore' modes. It also
    changes the 'moduleNameingPattern', 'includeOriginalPath', and
    'convertToCamelCase' options to be required. The 'mode' option defaults
    to 'object'.

--- src/options.ts
+++ src/options.ts
@@ -1,34 +1,64 @@
-import type { RequiredDeep, SetOptional } from 'type-fest';
+import type { PartialDeep, RequiredDeep, SetOptional } from 'type-fest';
 import { defu } from 'defu';
 
 /**
  * Options for the cssModules preprocessor
  */
-export type Options = {
+export type Options = PartialDeep<{
 	/*
 	* The pattern to use for module naming
 	* @see https://lightningcss.dev/css-modules.html
 	*/
-	moduleNameingPattern?: string;
+	moduleNameingPattern: string;
 
 	/*
 	* Whether to include the original path in the css module
 	* @default true
 	*/
-	includeOriginalPath?: boolean;
+	includeOriginalPath: boolean;
 
 	/*
 	* Whether to convert the module name to camel case
 	* @default true
 	*/
-	convertToCamelCase?: boolean;
-};
+	convertToCamelCase: boolean;
+
+	/*
+	* Whether to use the object mode or underscore mode
+	* underscore mode is tree-shakable
+	* @default 'object'
+	*
+	* @example
+	* for object mode
+	* ```svelte
+	* <script>
+	*   import styles from './foo.module.css';
+	*   // converted to `const s = { foo: 'foo' }`
+	* </script>
+	*
+	* <div class={styles.foo}></div>
+	* ```
+	*
+	* @example
+	* for underscore mode
+	* ```svelte
+	* <script>
+	*   import styles from './foo.module.css';
+	*   // converted to `const styles_foo = 'foo'`
+	* </script>
+	*
+	* <div class={styles_foo}></div>
+	* ```
+	*/
+	mode: 'object' | 'underscore';
+}>;
 
 export type ResolvedOptions = SetOptional<RequiredDeep<Options>, 'moduleNameingPattern'>;
 
 const defaultOptions = {
 	includeOriginalPath: true,
 	convertToCamelCase: true,
+	mode: 'object',
 } as const satisfies ResolvedOptions;
 
 export function resolveOptions(options: Options): Readonly<ResolvedOptions> {
